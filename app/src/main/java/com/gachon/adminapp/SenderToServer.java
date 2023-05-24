aa126cb07d497d0c1b850d57a7018936cd945175
mingy1206 <mingy837@gmail.com>
2023-05-25 00:45:23 +0900

    변수 타입과 이름 변경

--- app/src/main/java/com/gachon/adminapp/SenderToServer.java
+++ app/src/main/java/com/gachon/adminapp/SenderToServer.java
@@ -19,13 +19,13 @@ public class SenderToServer {
                 //selectLocationActivity에서 spinner에서 고른 값을 저장 후 가져옴
                 String floor = SelectLocationActivity.Floor;
                 String rp = SelectLocationActivity.RP;
-                String name = "임시 이름";
+                String place = "임시 이름";
                 
                 //빈 arraylist에 집어넣어줌
                 //constructor에서 받아온 arraylist는
                 // scanWifiActivity에서 저장한 5개의 AP 정보를 WifiDTO 형식으로 담은 Arraylist이다
                 nodeList = new ArrayList<>();
-                nodeList.add(new node(floor, rp, name, arrayList));
+                nodeList.add(new node(floor, rp, place, arrayList));
 
         }
         public static void send() {
--- app/src/main/java/com/gachon/adminapp/node.java
+++ app/src/main/java/com/gachon/adminapp/node.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 
 //전달하는 노드의 json 형식
 class node {
-    String floor;
+    int floor;
     String rp;
-    String name;
+    String place;
     ArrayList<WifiDTO> aps;
 
-    public node(String floor, String rp, String name, ArrayList arrayList) {
-        this.floor = floor;
+    public node(String floor, String rp, String place, ArrayList arrayList) {
+
+        if(floor.equals("4F")) this.floor = 4;
+        else if(floor.equals("5F")) this.floor = 5;
+        else this.floor = 0;
+
         this.rp = rp;
-        this.name = name;
+        this.place = place;
         this.aps = arrayList;
     }
 }
